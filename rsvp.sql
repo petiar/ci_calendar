85731e40e991c64bcc217fe00d1e113d6383d78c
vagrant <vagrant@php7dev.puphpet>
2018-03-03 16:46:32 +0000

    RSVP table schema added.

---
+++ rsvp.sql
@@ -0,0 +1,24 @@
+-- Adminer 4.6.2 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+DROP TABLE IF EXISTS `rsvp`;
+CREATE TABLE `rsvp` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `username` varchar(16) NOT NULL,
+      `eventid` varchar(64) NOT NULL,
+      `firstname` varchar(32) NOT NULL,
+      `lastname` varchar(32) NOT NULL,
+      `email` varchar(64) NOT NULL,
+      `phone` varchar(32) NOT NULL,
+      `speaker` tinyint(4) NOT NULL,
+      `connect` tinyint(4) NOT NULL,
+      `address` text NOT NULL,
+      `days` text NOT NULL,
+      `comment` text,
+      PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2018-03-03 16:45:28
