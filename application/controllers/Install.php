00e8cef57bf01f0f5b7bcbe00daad72c2bd7ec0a
Peter Lachky <petiar@petiar.sk>
2018-03-06 17:34:30 +0000

    Removing incopatible SQL commands.

--- application/controllers/Install.php
+++ application/controllers/Install.php
@@ -13,24 +13,26 @@ class Install extends CI_Controller {
   }
 
   public function index() {
-    $this->db->query('
-DROP TABLE IF EXISTS `rsvp`;
-CREATE TABLE `rsvp` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(16) NOT NULL,
-  `eventid` varchar(64) NOT NULL,
-  `firstname` varchar(32) NOT NULL,
-  `lastname` varchar(32) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `phone` varchar(32) NOT NULL,
-  `speaker` tinyint(4) NOT NULL,
-  `connect` tinyint(4) NOT NULL,
-  `date_from` date NOT NULL,
-  `date_to` date NOT NULL,
-  `address` text NOT NULL,
-  `comment` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-    ');
+    $query = <<<SQL
+DROP TABLE IF EXISTS rsvp;
+CREATE TABLE rsvp (
+  id int(11) SERIAL,
+  username varchar(16) NOT NULL,
+  eventid varchar(64) NOT NULL,
+  firstname varchar(32) NOT NULL,
+  lastname varchar(32) NOT NULL,
+  email varchar(64) NOT NULL,
+  phone varchar(32) NOT NULL,
+  speaker integer NOT NULL,
+  connect integer NOT NULL,
+  date_from date NOT NULL,
+  date_to date NOT NULL,
+  address text NOT NULL,
+  comment text,
+  PRIMARY KEY (id)
+);
+SQL;
+
+    $this->db->query($query);
   }
 }
