70d16d210ade392fad29487b7154eed53249bf71
Peter Lachky <petiar@petiar.sk>
2018-03-06 14:39:36 +0000

    Database schema script updated.

--- rsvp.sql
+++ rsvp.sql
@@ -1,3 +1,7 @@
+-- noinspection SqlDialectInspectionForFile
+
+-- noinspection SqlNoDataSourceInspectionForFile
+
 -- Adminer 4.6.2 MySQL dump
 
 SET NAMES utf8;
@@ -5,20 +9,21 @@ SET time_zone = '+00:00';
 
 DROP TABLE IF EXISTS `rsvp`;
 CREATE TABLE `rsvp` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `username` varchar(16) NOT NULL,
-      `eventid` varchar(64) NOT NULL,
-      `firstname` varchar(32) NOT NULL,
-      `lastname` varchar(32) NOT NULL,
-      `email` varchar(64) NOT NULL,
-      `phone` varchar(32) NOT NULL,
-      `speaker` tinyint(4) NOT NULL,
-      `connect` tinyint(4) NOT NULL,
-      `address` text NOT NULL,
-      `days` text NOT NULL,
-      `comment` text,
-      PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `eventid` varchar(64) NOT NULL,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `phone` varchar(32) NOT NULL,
+  `speaker` tinyint(4) NOT NULL,
+  `connect` tinyint(4) NOT NULL,
+  `date_from` date NOT NULL,
+  `date_to` date NOT NULL,
+  `address` text NOT NULL,
+  `comment` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2018-03-03 16:45:28
+-- 2018-03-06 14:36:30
