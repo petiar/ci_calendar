35061f4eaf70f0b42c0c3558fb7273f9a986aafa
Peter Lachky <petiar@petiar.sk>
2018-03-07 14:20:22 +0000

    Calendar ID changed.

--- application/config/spm.php
+++ application/config/spm.php
@@ -8,5 +8,5 @@ $config['spm_application_name'] = 'SPM Events Calendar';
 $config['spm_superuser'] = 'administrator';
 // $config['spm_calendar_id'] = 'participatorymedicine.org_6j9ak1oh6bhdqidc2ph3clf4es@group.calendar.google.com'; // dev test public calendar
 // $config['spm_calendar_id'] = '6d9bnutuoo30dgnivq738hnnk0@group.calendar.google.com'; // dev test public calendar
-$config['spm_calendar_id'] = 'participatorymedicine.org_h998jekm9hj03fb8h567iklo90@group.calendar.google.com'; // production calendar
+$config['spm_calendar_id'] = 'emailcentral%40participatorymedicine.org'; // production calendar
 $config['spm_developer_key'] = 'AIzaSyCxgHQ4b423qsoWkIADcjRmvRTWXvZTguc';
