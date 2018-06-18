b8ba4be13d8f65291d9c2d2b8552ad23d22bea5f
Peter Lachky <peter.lachky@firstlinesoftware.com>
2018-06-18 18:08:36 +0200

    Tiny config change

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://participatorymedicine.org/meetingplace/';
+$config['base_url'] = 'https://participatorymedicine.org/meetingplace/';
 
 /*
 |--------------------------------------------------------------------------
